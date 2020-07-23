cf181ad5759512e14f7c461da968e1181a465e38
Mohammed Shifreen <mshifreen@gmail.com>
2020-07-23 08:17:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -37,8 +37,7 @@
                             </div>
 
                             <div class="col-md-4">
-                                <?php //get_template_part( 'template-parts/content/trending-widget' ); ?>
-                                <?php get_template_part( 'template-parts/content/offers-widget' ); ?>
+                                <?php get_template_part( 'template-parts/content/trending-widget' ); ?>
                             </div>
                         </div>
                     <?php endwhile; ?>
