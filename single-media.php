a491836ef0557671c9b67a35d5283120971031a1
Mohammed Shifreen <mshifreen@gmail.com>
2020-08-06 10:01:41 +0530

    Update to single-media.php
    Hiding the featured image for infographics single page.

--- single-media.php
+++ single-media.php
@@ -6,35 +6,35 @@
 
 <?php get_header(); ?>
 
-    <div class="container-fluid">
-        <section class="page-section">
-            <div class="page-center">
-                <?php while (have_posts()) : the_post(); ?>
-                    <?php
-					$columnist         = getTheColumnist();
-					$columnistPicture  = getTheColumnistPictureUrl($columnist);
-					$author            = getPostAuthorName();
-					$authorDisplayName = isEditorInChief($author) ? $author . ', Editor-In-Chief' : $author;
-					$category = get_the_category();
-					?>
+<div class="container-fluid">
+    <section class="page-section">
+        <div class="page-center">
+            <?php while (have_posts()) : the_post(); ?>
+                <?php
+                $columnist         = getTheColumnist();
+                $columnistPicture  = getTheColumnistPictureUrl($columnist);
+                $author            = getPostAuthorName();
+                $authorDisplayName = isEditorInChief($author) ? $author . ', Editor-In-Chief' : $author;
+                $category          = get_the_category();
+                ?>
 
-                    <?php theBreadcrumb(); ?>
+                <?php theBreadcrumb(); ?>
 
-                    <header class="page-header">
-                        <h1 class="page-title"><?php the_title(); ?></h1>
+                <header class="page-header">
+                    <h1 class="page-title"><?php the_title(); ?></h1>
 
-                        <?php if (!isPostInBrief()) : ?>
-                            <p class="lead story-intro text-muted mb-0">
-                                <?php echo get_the_excerpt(); ?>
-                            </p>
-                        <?php endif; ?>
+                    <?php if (!isPostInBrief()) : ?>
+                        <p class="lead story-intro text-muted mb-0">
+                            <?php echo get_the_excerpt(); ?>
+                        </p>
+                    <?php endif; ?>
 
-                        <div class="media page-meta mt-3">
-                            <img src="<?php echo esc_url($columnistPicture); ?>"
-                                 class="align-self-center author-image rounded-circle mr-3"
-                                 alt="<?php echo esc_html($author); ?>" width="50" height="50">
+                    <div class="media page-meta mt-3">
+                        <img src="<?php echo esc_url($columnistPicture); ?>"
+                             class="align-self-center author-image rounded-circle mr-3"
+                             alt="<?php echo esc_html($author); ?>" width="50" height="50">
 
-                            <div class="media-body">
+                        <div class="media-body">
                                 <span class="story-author d-block">
                                     <?php if ($columnist instanceof WP_Term) : ?>
                                         <strong><a href="<?php echo esc_url(get_category_link($columnist)); ?>">
@@ -44,46 +44,48 @@
                                     <?php endif; ?>
                                 </span>
 
-                                <time class="story-date published" datetime="<?php echo esc_attr(get_the_date('C')); ?>" itemprop="datePublished">
-                                    <?php echo esc_html(get_the_date('l F j, Y')); ?>
-                                </time>
-                            </div>
-                        </div>
-
-                        <div class="page-share mt-2">
-                            <?php echo do_shortcode('[addtoany]'); ?>
-                        </div>
-                    </header>
-
-                    <div class="page-content">
-                        <?php if ($category[0]->slug !== 'videos') : ?>
-                            <?php $images = get_attached_media('image'); ?>
-
-                            <div class="row row-cols-1 row-cols-md-3 post-gallery">
-                                <?php foreach ($images as $image) : ?>
-                                    <?php
-                                    $fullSrc = wp_get_attachment_image_src($image->ID, 'full');
-                                    $wideSrc = wp_get_attachment_image_src($image->ID, 'wide');
-                                    ?>
-
-                                    <div class="col mb-4">
-                                        <a href="<?php echo $fullSrc[0]; ?>" class="card">
-                                            <img src="<?php echo $wideSrc[0]; ?>" class="d-block w-100" alt="<?php the_title(); ?>">
-                                        </a>
-                                    </div>
-                                <?php endforeach; ?>
-                            </div>
-                        <?php endif; ?>
-
-                        <div class="row">
-                            <div class="col-lg-8 offset-lg-2">
-                                <?php the_content(); ?>
-                            </div>
+                            <time class="story-date published" datetime="<?php echo esc_attr(get_the_date('C')); ?>"
+                                  itemprop="datePublished">
+                                <?php echo esc_html(get_the_date('l F j, Y')); ?>
+                            </time>
                         </div>
                     </div>
-                <?php endwhile; ?>
-            </div>
-        </section>
-    </div>
 
-<?php get_footer();?>
+                    <div class="page-share mt-2">
+                        <?php echo do_shortcode('[addtoany]'); ?>
+                    </div>
+                </header>
+
+                <div class="page-content">
+                    <?php if ($category[0]->slug == 'pictures') : ?>
+                        <?php $images = get_attached_media('image'); ?>
+
+                        <div class="row row-cols-1 row-cols-md-3 post-gallery">
+                            <?php foreach ($images as $image) : ?>
+                                <?php
+                                $fullSrc = wp_get_attachment_image_src($image->ID, 'full');
+                                $wideSrc = wp_get_attachment_image_src($image->ID, 'wide');
+                                ?>
+
+                                <div class="col mb-4">
+                                    <a href="<?php echo $fullSrc[0]; ?>" class="card">
+                                        <img src="<?php echo $wideSrc[0]; ?>" class="d-block w-100"
+                                             alt="<?php the_title(); ?>">
+                                    </a>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <div class="row">
+                        <div class="col-lg-8 offset-lg-2">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </section>
+</div>
+
+<?php get_footer(); ?>
